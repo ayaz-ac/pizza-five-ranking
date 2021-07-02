af6de099e4cf34a82a37d4a6895fce0f08451b0c
= <=>
2021-07-02 19:15:25 +0200

    🗃 Added Pizza seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/MethodLength
+def create_pizza(file_name, sauce)
+  pizza_five_url = 'https://www.fivepizzaoriginal.com'
+  csv_file = File.read("#{Rails.root}/lib/assets/#{file_name}.csv")
+  csv = CSV.parse(csv_file, headers: true)
+  csv.each do |row|
+    Pizza.find_or_create_by(
+      title: row['title'],
+      composition: row['composition'],
+      sauce: sauce,
+      img_url: pizza_five_url + row['image-src']
+    )
+  end
+end
+# rubocop:enable Metrics/MethodLength
+
+warn '==> Seeding Pizzas'
+
+# Create tomatoe pizzas
+create_pizza('sauce_tomate', 'tomate')
+
+# Create creme_fraiche pizza
+create_pizza('creme_fraiche', 'creme_fraiche')
+
+# Create originale pizzas
+create_pizza('sauce_originale', 'originale')
