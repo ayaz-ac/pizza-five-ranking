c2942457717534e011e29fbc710daf7b3c908f47
= <=>
2021-07-02 18:30:37 +0200

    🗃 Added pizza sauces

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_155924) do
+ActiveRecord::Schema.define(version: 2021_07_02_161532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "pizzas", force: :cascade do |t|
-    t.string "title"
-    t.text "composition"
-    t.text "img_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "pizzas" because of following StandardError
+#   Unknown type 'pizza_sauce' for column 'sauce'
 
 end
